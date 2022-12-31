3a9cabff878d27a6c6ea0df5918ffa860569cc24
endlmk <1269467+endlmk@users.noreply.github.com>
2022-12-31 21:50:33 +0900

    Add insert coin test.

--- apps/tddVendingMachineErlang/src/vendingMachine.erl
+++ apps/tddVendingMachineErlang/src/vendingMachine.erl
@@ -1,5 +1,19 @@
 -module(vendingMachine).
 
--export([buy/0]).
+-export([buy/0, initMachine/0]).
 
 buy() -> "coke".
+
+initMachine() -> create(0).
+
+create(Amount) -> 
+    fun(Message) ->
+        case Message of
+            "buy" -> 
+                if 
+                    Amount >= 100 -> "coke";
+                    true -> ""
+                end;
+            "insert100yen" -> create(Amount + 100)
+        end
+    end.
--- test/testApp.erl
+++ test/testApp.erl
@@ -3,3 +3,11 @@
 
 buy_test() -> 
     ?assertEqual("coke", vendingMachine:buy()).
+
+insertCoin_test() ->
+    VendingM = vendingMachine:initMachine(),
+    ?assertEqual("", VendingM("buy")),
+    VendingM = vendingMachine:initMachine(),
+    VendingM2 = VendingM("insert100yen"),
+    ?assertEqual("coke", VendingM2("buy")).
+
