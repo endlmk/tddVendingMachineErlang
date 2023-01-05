e5bdec2dd1b8d63749b3491f6dce5c8ed84ce47a
endlmk <1269467+endlmk@users.noreply.github.com>
2023-01-05 22:50:30 +0900

    Passed test for oolong tea.

--- apps/tddVendingMachineErlang/src/vendingMachine.erl
+++ apps/tddVendingMachineErlang/src/vendingMachine.erl
@@ -1,19 +1,17 @@
 -module(vendingMachine).
 
--export([buy/0, initMachine/0]).
-
-buy() -> "coke".
+-export([initMachine/0]).
 
 initMachine() -> create(0).
 
 create(Amount) -> 
     fun(Message) ->
         case Message of
-            "buy" -> 
+            ["buy", Drink]  -> 
                 if 
-                    Amount >= 100 -> {"coke", create(Amount - 100)};
+                    Amount >= 100 -> {Drink, create(Amount - 100)};
                     true -> {"", create(Amount)}
                 end;
-            "insert100yen" -> create(Amount + 100)
+            ["insert", "100yen"] -> create(Amount + 100)
         end
     end.
--- test/testApp.erl
+++ test/testApp.erl
@@ -1,16 +1,18 @@
 -module(testApp).
 -include_lib("eunit/include/eunit.hrl").
 
-buy_test() -> 
-    ?assertEqual("coke", vendingMachine:buy()).
-
 insertCoin_test() ->
     VM = vendingMachine:initMachine(),
-    {Drink1, VM1} = VM("buy"),
+    {Drink1, VM1} = VM(["buy", "Coke"]),
     ?assertEqual("", Drink1),
-    VM2 = VM1("insert100yen"),
-    {Drink2, VM3} = VM2("buy"), 
-    ?assertEqual("coke", Drink2),
-    {Drink3, _} = VM3("buy"), 
+    VM2 = VM1(["insert", "100yen"]),
+    {Drink2, VM3} = VM2(["buy", "Coke"]), 
+    ?assertEqual("Coke", Drink2),
+    {Drink3, _} = VM3(["buy", "Coke"]), 
     ?assertEqual("", Drink3).
 
+buyOolong_test() ->
+    VM1 = vendingMachine:initMachine(),
+    VM2 = VM1(["insert", "100yen"]),
+    {Drink1, VM1} = VM2(["buy", "Oolong"]),
+    ?assertEqual("Oolong", Drink1).
